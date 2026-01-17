54932f05a911e7bdea01642527f1c727af611ba2
Carlos López <genetita@gmail.com>
2026-01-17 16:55:27 +0100

    Ajustar nombres de columnas a especificación exacta (NUMEROAT, RESPONSABLEID, etc)

--- app/models/expedientes.py
+++ app/models/expedientes.py
@@ -5,11 +5,11 @@ class Expediente(db.Model):
     __tablename__ = 'expedientes'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    numero_at = db.Column(db.Integer, nullable=False, unique=True)
-    responsable_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
-    tipo_expediente_id = db.Column(db.Integer, db.ForeignKey('tipos_expedientes.id'))
-    heredado = db.Column(db.Boolean)
-    proyecto_id = db.Column(db.Integer, db.ForeignKey('proyectos.id'), nullable=False, unique=True)
+    numero_at = db.Column("NUMEROAT", db.Integer, nullable=False, unique=True)
+    responsable_id = db.Column("RESPONSABLEID", db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+    tipo_expediente_id = db.Column("TIPOEXPEDIENTEID", db.Integer, db.ForeignKey('tipos_expedientes.id'))
+    heredado = db.Column("HEREDADO", db.Boolean)
+    proyecto_id = db.Column("PROYECTOID", db.Integer, db.ForeignKey('proyectos.id'), nullable=False, unique=True)
     
     def __repr__(self):
         return f'<Expediente {self.numero_at}>'
