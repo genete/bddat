0e2119b67aad16bb2305ea64f66787d4bc8eb7df
Carlos López <genetita@gmail.com>
2026-01-17 16:55:20 +0100

    Importar Proyecto y TipoExpediente en __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,4 @@
 from app.models.expedientes import Expediente
 from app.models.usuarios import Usuario
+from app.models.proyectos import Proyecto
+from app.models.tipos_expedientes import TipoExpediente
