79bebe55c5b77a1033564dbcdba81a076c9a6f5e
Carlos López <genetita@gmail.com>
2026-01-22 19:10:42 +0100

    [MODELO] Actualizar __init__.py para exportar TipoSolicitud, TipoFase y SolicitudTipo

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,5 +3,18 @@ from app.models.usuarios import Usuario, Rol
 from app.models.proyectos import Proyecto
 from app.models.tipos_expedientes import TipoExpediente
 from app.models.tipos_ia import TipoIA
+from app.models.tipos_solicitudes import TipoSolicitud
+from app.models.tipos_fases import TipoFase
+from app.models.solicitudes_tipos import SolicitudTipo
 
-__all__ = ['Expediente', 'Usuario', 'Rol', 'Proyecto', 'TipoExpediente', 'TipoIA']
+__all__ = [
+    'Expediente',
+    'Usuario',
+    'Rol',
+    'Proyecto',
+    'TipoExpediente',
+    'TipoIA',
+    'TipoSolicitud',
+    'TipoFase',
+    'SolicitudTipo'
+]
